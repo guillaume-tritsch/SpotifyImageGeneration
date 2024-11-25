dd06453b2e1bf48740afcb3f3765960bf9b7eb83
guillaume-tritsch <guillaume.tritsch.64@gmail.com>
2024-11-26 00:03:06 +0100

    try cors security

--- scripts/loader.js
+++ scripts/loader.js
@@ -2,7 +2,7 @@ async function loadNavbar() {
   const response = await fetch("./pages/components/header.html");
   if (response.ok) {
     const navbarHtml = await response.text();
-    document.getElementById("header").innerHTML = navbarHtml;
+    document.getElementById("page-header").innerHTML = navbarHtml;
   } else {
     console.error("Navbar could not be loaded");
   }
