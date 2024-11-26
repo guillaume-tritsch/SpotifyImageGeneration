44d713ca6a72df39f0193915f11466d1363754e6
guillaume-tritsch <guillaume.tritsch.64@gmail.com>
2024-11-26 10:01:49 +0100

    connection

---
+++ scripts/api.js
@@ -0,0 +1,131 @@
+class API {
+  #token = null;
+
+  isDevMode = true;
+  devCallbackURI = "http://localhost:5500/callback.html";
+  prodCallbackURL =
+    "https://guillaume-tritsch.github.io/SpotifyImageGeneration/pages/callback";
+  spotifyData = {
+    clientID: "5dbf2ff92be149ca98e8198088566890",
+  };
+
+  baseURL = "https://accounts.spotify.com/";
+  scope = "user-top-read";
+
+  promiseAllObtention;
+
+  userData;
+  userTopTracks;
+
+  async spotifyConnect() {
+    window.location.href =
+      this.baseURL +
+      "authorize?client_id=" +
+      this.spotifyData.clientID +
+      "&response_type=token&redirect_uri=" +
+      (this.isDevMode ? this.devCallbackURI : this.prodCallbackURL) +
+      "&scope=" +
+      this.scope;
+  }
+
+  isConnected() {
+    return this.#token != null;
+  }
+
+  setToken(token) {
+    sessionStorage.setItem("access_token", token);
+    window.location.href = "http://localhost:5500/index.html";
+  }
+
+  deleteToken() {
+    sessionStorage.removeItem("access_token");
+    window.location.href = "http://localhost:5500/index.html";
+  }
+
+  clearAllData() {
+    sessionStorage.clear();
+  }
+
+  constructor() {
+    this.#token = sessionStorage.getItem("access_token");
+
+    let dataToObtains = [];
+
+    if (this.isConnected() && !sessionStorage["user_data"]) {
+      dataToObtains.push(this.fetchUserData());
+    }
+
+    if (this.isConnected() && !sessionStorage["top_tracks"]) {
+      dataToObtains.push(this.fetchTopTracks(50));
+    }
+
+    this.promiseAllObtention = Promise.all(dataToObtains).then(() => {
+      this.userTopTracks = JSON.parse(sessionStorage.getItem("top_tracks"));
+      this.userData = JSON.parse(sessionStorage.getItem("user_data"));
+    });
+  }
+
+  getUserName() {
+    return this.userData['display_name'];
+  }
+
+  async fetchTopTracks(limit) {
+    const url = `https://api.spotify.com/v1/me/top/tracks?time_range=long_term&limit=${limit}`;
+    try {
+      const response = await fetch(url, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${this.#token}`,
+          "Content-Type": "application/json",
+        },
+      });
+      console.log(response)
+      if (!response.ok) {
+        throw new Error("Error fetching top tracks");
+      }
+
+      const data = await response.json();
+      sessionStorage.setItem("top_tracks", JSON.stringify(data));
+    } catch (error) {
+      console.error("Erreur lors de la récupération des morceaux:", error);
+    }
+  }
+
+  async fetchUserData() {
+    const url = "https://api.spotify.com/v1/me";
+    try {
+      const response = await fetch(url, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${this.#token}`,
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Error fetching user data");
+      }
+
+      const data = await response.json();
+      console.log(JSON.stringify(data));
+      sessionStorage.setItem("user_data", JSON.stringify(data));
+    } catch (error) {
+      console.error("Erreur lors des données utilisateurs:", error);
+    }
+  }
+
+  async isAllDataObtained() {
+    return this.promiseAllObtention;
+  }
+}
+
+const APIInstance = new API();
+
+function connect() {
+  APIInstance.spotifyConnect();
+}
+
+function deconnect() {
+  APIInstance.clearAllData();
+  APIInstance.deleteToken();
+}
--- scripts/loader.js
+++ scripts/loader.js
@@ -1,5 +1,7 @@
+// LOAD COMPONENTS
+
 async function loadNavbar() {
-  const response = await fetch("./pages/components/header.html");
+  const response = await fetch("./components/header.html");
   if (response.ok) {
     const navbarHtml = await response.text();
     document.getElementById("page-header").innerHTML = navbarHtml;
@@ -8,6 +10,72 @@ async function loadNavbar() {
   }
 }
 
-console.log("eee");
-loadNavbar();
-console.log("ddd");
+async function loadFooter() {
+  const response = await fetch("./components/footer.html");
+  if (response.ok) {
+    const footerHtml = await response.text();
+    document.getElementById("page-footer").innerHTML = footerHtml;
+  } else {
+    console.error("Footer could not be loaded");
+  }
+}
+
+let isAllLoaded = [loadFooter(), loadNavbar()];
+
+Promise.all(isAllLoaded).then(() => {
+  let isNotLoadingHTMLElement =
+    document.getElementsByClassName("if-not-loading");
+
+  for (let el of isNotLoadingHTMLElement) {
+    el.style.display = "none";
+  }
+});
+
+// Apply javascript utilities when everything has load
+Promise.all([APIInstance.isAllDataObtained(), ...isAllLoaded])
+  .then(() => {
+    let isLoadingHTMLElement = document.getElementsByClassName("if-loading");
+
+    for (let el of isLoadingHTMLElement) {
+      el.remove();
+    }
+
+    let isNotLoadingHTMLElement =
+      document.getElementsByClassName("if-not-loading");
+
+    for (let el of isNotLoadingHTMLElement) {
+      el.style.display = "block";
+    }
+
+    // Connected / Disconnected
+    let isConnectedHTMLElement =
+      document.getElementsByClassName("if-connected");
+    let isNotConnectedHTMLElement =
+      document.getElementsByClassName("if-not-connected");
+
+    for (let el of isConnectedHTMLElement) {
+      if (!APIInstance.isConnected()) {
+        el.remove();
+      }
+    }
+
+    for (let el of isNotConnectedHTMLElement) {
+      if (APIInstance.isConnected()) {
+        el.remove();
+      }
+    }
+
+    // Users data
+    let userImageHTMLElement =
+      document.getElementsByClassName("data-user-image");
+
+    let userNameHTMLElement = document.getElementsByClassName("data-user-name");
+    console.log(userNameHTMLElement.length);
+    for (let el of userNameHTMLElement) {
+      console.log(APIInstance.getUserName());
+      el.innerText = APIInstance.getUserName();
+    }
+  })
+  .catch((error) => {
+    console.error(error);
+  });
